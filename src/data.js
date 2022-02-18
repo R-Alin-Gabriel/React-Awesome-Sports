73eca48ea15abd7a53d35441b82a162c0b1330e1
R-Alin-Gabriel <rudareanu_alin@yahoo.com>
2022-02-18 15:00:09 +0200

    first commit

---
+++ src/data.js
@@ -0,0 +1,70 @@
+const menu = [
+    {
+      id: 1,
+      title: 'Snowboarding',
+      category: 'Board sports',
+            img: './images/Snowboarding.jpg',
+      desc: `A recreational and competitive activity that involves descending a snow-covered slope while standing on a snowboard that is almost always attached to a rider's feet. It features in the Winter Olympic Games and Winter Paralympic Games`,
+    },
+    {
+      id: 2,
+      title: 'skydiving',
+      category: 'Air sports',
+      img: './images/skydiving.jpg',
+      desc: `Skydiving, is a method of transiting from a high point in the atmosphere to the surface of Earth with the aid of gravity, involving the control of speed during the descent using a parachute or parachutes.
+
+      For human skydiving, it may involve a phase of more or less free-falling (the skydiving segment) which is a period when the parachute has not yet been deployed and the body gradually accelerates to terminal velocity.  `,
+    },
+    {
+      id: 3,
+      title: 'Dirt jumping',
+      category: 'Bicycle Sports',
+      img: './images/Dirt jumping.jpg',
+      desc: `Dirt jumping is the practice of riding bikes over jumps made of dirt or soil and becoming airborne. Dirt Jumping evolved alongside BMX racing and is similar to BMX or mountain bike racing in that the rider jumps off of mounds of dirt, usually performing a midair trick in between. It differs in that the jumps are usually much larger and designed to lift the rider higher into the air. Additionally, the goal is not to complete the course with the fastest time, but rather to perform the tricks with the style. Dirt jumping can be performed on BMX bikes or specialized mountain bikes known simply as "dirt jumpers". `,
+    },
+    {
+      id: 4,
+      title: 'Wakeboarding',
+      category: 'Board sports',
+      img: './images/Wakeboarding.jpg',
+      desc: `Wakeboarding is a water sport in which the rider, standing on a wakeboard (a short board with foot bindings), is towed behind a motorboat across its wake and especially up off the crest in order to perform aerial maneuvers. A hallmark of wakeboarding is the attempted performance of midair tricks. Wakeboarding was developed from a combination of water skiing, snowboarding and surfing techniques.  `,
+    },
+    {
+      id: 5,
+      title: 'Competition aerobatics',
+      category: 'Air sports',
+      img: './images/aerobatics.jpg',
+      desc: `An air sport in which judges rate the skill of pilots performing aerobatic flying. It is practiced in both piston-powered single-engine airplanes and also gliders.
+
+      An aerobatic competition is sanctioned by a national aero club, its designee, or in the case of international competitions, by CIVA, the Commission Internationale Voltige Aerienne, which is a constituent body of the Fédération Aéronautique Internationale (FAI). The sanctioning body establishes the rules that apply to the competition, including entry qualifications for all participants, operating procedures, and judging criteria. `,
+    },
+    {
+      id: 6,
+      title: 'Freestyle BMX',
+      category: 'Bicycle Sports',
+      img: './images/Freestyle BMX.jpg',
+      desc: `Freestyle BMX is bicycle motocross stunt riding on BMX bikes. It is an extreme sport descended from BMX racing that consists of five disciplines: street, park, vert, trails, and flatland. In June 2017, the International Olympic Committee announced that freestyle park was to be added as an Olympic event to the 2020 Summer Olympics.`,
+    },
+    {
+      id: 7,
+      title: 'Skateboarding',
+      category: 'Board sports',
+      img: './images/Skateboarding.jpg',
+      desc: `An action sport originating in the United States that involves riding and performing tricks using a skateboard, as well as a recreational activity, an art form, an entertainment industry job, and a method of transportation. Skateboarding has been shaped and influenced by many skateboarders throughout the years. A 2009 report found that the skateboarding market is worth an estimated $4.8 billion in annual revenue, with 11.08 million active skateboarders in the world. In 2016, it was announced that skateboarding will be represented at the 2020 Summer Olympics in Tokyo, for both male and female teams `,
+    },
+    {
+      id: 8,
+      title: 'Paragliding',
+      category: 'Air sports',
+      img: './images/paragliding.jpg',
+      desc: `Paragliding is the recreational and competitive adventure sport of flying paragliders: lightweight, free-flying, foot-launched glider aircraft with no rigid primary structure. The pilot sits in a harness or lies supine in a cocoon-like 'speed bag' suspended below a fabric wing. Wing shape is maintained by the suspension lines, the pressure of air entering vents in the front of the wing, and the aerodynamic forces of the air flowing over the outside.  `,
+    },
+    {
+      id: 9,
+      title: 'Road bicycle racing',
+      category: 'Bicycle Sports',
+      img: './images/Road bicycle racing.jpg',
+      desc: `Road bicycle racing is the cycle sport discipline of road cycling, held on paved roads. Road racing is the most popular professional form of bicycle racing, in terms of numbers of competitors, events and spectators. The two most common competition formats are mass start events, where riders start simultaneously (though sometimes with a handicap) and race to a set finish point; and time trials, where individual riders or teams race a course alone against the clock. Stage races or "tours" take multiple days, and consist of several mass-start or time-trial stages ridden consecutively. `,
+    },
+  ];
+  export default menu;
